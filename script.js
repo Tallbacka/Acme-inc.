28bf3f8f21c9d9d6e211a01f04d086c6a48d945b
Jimmy Johansson <jim-joh@outlook.com>
2019-09-09 00:22:23 +0200

    loginPage done

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    
+    
+    function create() {
+    var h1 = document.createElement('h1');
+    h1.textContent = "New Heading!!!";
+    h1.setAttribute('class', 'note');
+    document.body.appendChild(h1);
+    }
