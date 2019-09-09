28078f31c9f3e32c7f12c386ff189dae23034ba6
Jimmy Johansson <jim-joh@outlook.com>
2019-09-09 12:29:57 +0200

    adding structure js

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+    const userName = "test";
+    const password = "1234";
+    var userLocal = localStorage.key[0].getItem;
+    var passLocal = localStorage.getItem[userLocal];
+    var userInput = document.getElementById("inputUserName").value;
+    var passInput = document.getElementById("inputPassword").value;
+
+
+    document.getElementById("onloadCheckLocalstorage").onload = function () {}
 
     
+    function onloadCheckLocalstorage (userLocal, passLocal) {
+        if (condition) {
+            
+            // goto index
+        }
+      }
+
+      document.getElementById("btnLogin").onclick = function validate(userName, password) {
+          if (userName === userInput && password === passInput) {
+            //   goto home
+          } else {
+            //   goto notValidated
+          }
+      }
     
+      function drawHome (param) {
+        //   create welcome/homepage
+        }
+
+
     function create() {
     var h1 = document.createElement('h1');
     h1.textContent = "New Heading!!!";
