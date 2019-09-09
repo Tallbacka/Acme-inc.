a5d40af82b4e77ee87bcf06686367ed0742a66a8
Jimmy Johansson <jim-joh@outlook.com>
2019-09-09 21:53:41 +0200

    fixed js to markup

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ const userKey = "acmeUser"; //Hardcoded values
 const PassKey = "acmePass"; //Hardcoded values
 var body = document.getElementById('onloadCheckLocalstorage');
 const wrapper = document.getElementById('wrapper');
+const header = document.getElementById('header');
 
 // checks if values are still stored in localstorage, if yes the user is passed onto homepage
 document.getElementById("onloadCheckLocalstorage").onload = function () {
 
     if (true) {
-        if (localStorage.getItem(userKey) > 0 && localStorage.getItem(PassKey) > 0) {
+        if (localStorage.getItem(userKey) !== 0 && localStorage.getItem(PassKey) !== 0) {
             removeChildElements(wrapper);
-            drawHome(wrapper);
+            drawHome();
             return false
         } else {
             removeChildElements(wrapper);
@@ -43,19 +44,16 @@ function login() {
 
 // removes al the childnodes in the wrapper div, and draw new elements for homepage
 function drawHome() {
-    var header = document.createElement('header')
     var h1 = document.createElement('h1');
     var ul = document.createElement('ul');
     var li = document.createElement('li');
     var a = document.createElement('a');
-    header.textContent = "Acme Inc."
     a.setAttribute('href', 'javascript:logout()');
     a.textContent = "Logga ut";
     h1.textContent = "Välkommen";
     li.appendChild(a);
     ul.appendChild(li);
     header.appendChild(ul)
-    // document.getElementById('header').appendChild(ul);
     wrapper.appendChild(h1);
 }
 
@@ -82,12 +80,14 @@ function insertToLocalstorage(userInput, passInput) {
 function logout() {
     localStorage.removeItem(userKey)
     localStorage.removeItem(PassKey)
+    removeChildElements(header)
     createIndex();
 
 }
 
 // redirects to index page
 function tryAgain() {
+    removeChildElements(wrapper);
     createIndex();
 }
 
@@ -98,8 +98,8 @@ function removeChildElements(parent) {
 }
 
 function createIndex() {
-    body.insertAdjacentHTML('afterbegin', '<header><h1 id="header">Acme inc.</h1></header>');
-    body.insertAdjacentHTML('beforeend', '<div id="wrapper"><h2 class="para">Logga in</h2><div><input type="text" id="inputUserName" placeholder="Anvandarnamn"></div>');
+    // header.insertAdjacentHTML('afterbegin', '<h1 id="header">Acme inc.</h1>');
+    wrapper.insertAdjacentHTML('beforeend', '<h2 class="para">Logga in</h2><div><input type="text" id="inputUserName" placeholder="Anvandarnamn">');
     wrapper.insertAdjacentHTML('beforeend', '<div><input type="text" id="inputPassword" placeholder="Losenord"></div>');
     wrapper.insertAdjacentHTML('beforeend', '<div><button onclick="login()">Logga in...</button></div>');
     wrapper.insertAdjacentHTML('beforeend', '<div id="containerRem"><input id="rememberMe" type="checkbox" name="cbRememberMe" id="cbRememberMe"><label for="cbRememberMe">Kom ihag mig!</label></div>');
